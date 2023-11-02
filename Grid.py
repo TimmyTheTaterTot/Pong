dcf4efc951ca0b2e98eec6adf3327f9ed1606696
TimmyTheTaterTot <zackd365@gmail.com>
2023-11-01 23:55:20 -0600

    undid the added space

--- Grid.py
+++ Grid.py
@@ -6,7 +6,6 @@ class Grid:
     2D grid with (x, y) int indexed internal storage
     Has .width .height size properties
     """
-    
 
     def __init__(self, width, height):
         self.height = height
